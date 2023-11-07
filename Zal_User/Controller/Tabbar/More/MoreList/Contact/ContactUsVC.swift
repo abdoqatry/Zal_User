f1636334056be5421effaa7897c9fed021cacfed
abdelhamed qatry <ios@delitechnoworks.com>
2023-11-07 23:23:35 +0200

    ghjkkl

--- Zal_User/Controller/Tabbar/More/MoreList/Contact/ContactUsVC.swift
+++ Zal_User/Controller/Tabbar/More/MoreList/Contact/ContactUsVC.swift
@@ -9,12 +9,35 @@ import UIKit
 
 class ContactUsVC: UIViewController {
 
+    @IBOutlet weak var sendBT: UIButton!
+    @IBOutlet weak var messageTV: UITextView!
+    @IBOutlet weak var mailTF: UITextField!
+    @IBOutlet weak var whatappImage: UIImageView!
+    @IBOutlet weak var twitterImage: UIImageView!
+    @IBOutlet weak var facebookImage: UIImageView!
+    @IBOutlet weak var gmailImage: UIImageView!
+    @IBOutlet weak var InstegramImage: UIImageView!
+    @IBOutlet weak var snapChatImage: UIImageView!
+    @IBOutlet weak var PhonNumTF: UITextField!
+    @IBOutlet var itemView: [UIView]!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        title = "Contact us".localize
+        setView()
+    }
+    
+    func setView(){
+        for i in itemView{
+            i.layer.cornerRadius = 20
+            i.layer.borderWidth = 0.5
+            i.layer.borderColor = UIColor.lightGray.cgColor
+        }
+    }
+    
+    @IBAction func sendButton(_ sender: UIButton) {
         
     }
     
     
-
+    
 }
